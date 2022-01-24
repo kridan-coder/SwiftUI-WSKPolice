03fc9c0bcfd62e15785ed6cfd9f19c01225298e8
Daniel <krivelevdaniil@gmail.com>
2022-01-24 10:30:39 +0700

    Initial Commit

---
+++ xx_wskpolice/xx_wskpoliceApp.swift
@@ -0,0 +1,17 @@
+//
+//  xx_wskpoliceApp.swift
+//  xx_wskpolice
+//
+//  Created by Daniel Krivelev on 24.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct xx_wskpoliceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
