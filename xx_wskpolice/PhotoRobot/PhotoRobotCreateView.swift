6a86248aa15e6ca89b66be9d9913a93b35e7e538
Daniel <krivelevdaniil@gmail.com>
2022-01-27 14:10:19 +0700

    swiftUI implementation for photorobot is done

--- xx_wskpolice/PhotoRobot/PhotoRobotCreateView.swift
+++ xx_wskpolice/PhotoRobot/PhotoRobotCreateView.swift
@@ -66,114 +66,44 @@ struct PageViewWithButtons<Page: View>: View {
   
   var body: some View {
     ZStack {
-      PageViewController(pages: pages, currentPage: $currentPage)
-      ScrollViewReader { proxy in
-        ScrollView(.horizontal, showsIndicators: false) {
-          ForEach(0..<pages.count, id: \.self) { index in
-            pages[index]
+      TabView(selection: $currentPage) {
+        ForEach(0..<pages.count) { index in
+          pages[index]
+        }
+      }
+      .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+      
+    
+      HStack {
+        if currentPage - 1 >= 0 {
+          Button {
+            withAnimation {
+              currentPage = currentPage - 1 < 0 ? pages.count - 1 : currentPage - 1
+            }
+          } label: {
+            Image("BackArrow")
           }
         }
-        .onChange(of: currentPage) { newValue in
-          proxy.scrollTo(newValue)
-        }
-        
-        
-      }
-      HStack {
-        Button("Previous") {
-          currentPage = currentPage + 1 >= pages.count ? 0 : currentPage + 1
-        }
+
         Spacer()
           .allowsHitTesting(false)
-        Button("Next") {
-          currentPage = currentPage - 1 < 0 ? pages.count - 1 : currentPage - 1
+        
+        if currentPage + 1 < pages.count {
+          Button {
+            withAnimation {
+              currentPage = currentPage + 1 >= pages.count ? 0 : currentPage + 1
+            }
+          } label: {
+            Image("NextArrow")
+          }
         }
-      }
-      
-      
-      
-      
-    }
-    
-  }
-}
 
-struct PageViewController<Page: View>: UIViewControllerRepresentable {
-  
-  var pages: [Page]
-  
-  @Binding var currentPage: Int
-  
-  func makeCoordinator() -> Coordinator {
-    Coordinator(self)
-  }
-  
-  func makeUIViewController(context: Context) -> UIPageViewController {
-    let controller = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal)
-    controller.dataSource = context.coordinator
-    controller.delegate = context.coordinator
-    return controller
-  }
-  
-  func updateUIViewController(_ uiViewController: UIPageViewController, context: Context) {
-    var safeCurrentPage = currentPage
-    if safeCurrentPage >= context.coordinator.controllers.count {
-      safeCurrentPage = 0
-    } else if safeCurrentPage < 0 {
-      safeCurrentPage = context.coordinator.controllers.count - 1
+      }
+      
+      
+      
+      
     }
-    
-    
-    currentPage = safeCurrentPage
-    
-    uiViewController.setViewControllers(
-      [context.coordinator.controllers[safeCurrentPage]], direction: .forward, animated: true)
-    
     
   }
-  
-  class Coordinator: NSObject, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
-    var parent: PageViewController
-    var controllers = [UIViewController]()
-    
-    init(_ pageViewController: PageViewController) {
-      parent = pageViewController
-      controllers = parent.pages.map { UIHostingController(rootView: $0) }
-    }
-    
-    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
-      guard let index = controllers.firstIndex(of: viewController) else {
-        return nil
-      }
-      if index == 0 {
-        return controllers.last
-      }
-      return controllers[index - 1]
-    }
-    
-    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
-      guard let index = controllers.firstIndex(of: viewController) else {
-        return nil
-      }
-      if index == controllers.count - 1 {
-        return controllers.first
-      }
-      return controllers[index + 1]
-    }
-    
-    
-    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
-      if completed,
-         let visibleViewController = pageViewController.viewControllers?.first,
-         let index = controllers.firstIndex(of: visibleViewController) {
-        parent.currentPage = index
-        print(parent.currentPage)
-      }
-    }
-    
-    
-    
-    
-  }
-  
 }
